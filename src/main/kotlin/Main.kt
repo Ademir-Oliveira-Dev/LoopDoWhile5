10258a9361d41a49754a97784943922be4ca37ad
Ademir Oliveira <ademir.sojunior@gmail.com>
2021-10-27 19:51:07 -0300

    atividade 5

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,44 @@
+fun main(args: Array<String>) {
+    /*5 - Crie um programa que leia um número do teclado até que encontre
+    um número igual a zero. No final, mostre:
+
+- A soma dos números digitados.
+- A média deles
+- O menor número digitado
+- O maior número digitado
+
+     */
+
+    var soma = 0
+    var contador = 0.0
+    var media = 0.0
+    var menorNum = 0
+    var maiorNum = 0
+
+    do {
+        print("Digite um número inteiro e positivo: ")
+        var numero = readLine()!!.toInt()
+        contador++
+        soma += numero
+        if (numero == 0){
+            contador--
+        }
+        media = soma/contador
+
+        if (numero!=0 && numero < menorNum){
+            menorNum = numero
+        }
+        if (numero > maiorNum){
+            maiorNum = numero
+
+        }
+
+    }while (numero != 0)
+    println("$contador")
+    println("A soma dos números é: $soma")
+    println("A média dos números é: $media")
+    println("O menor número é: $menorNum")
+    println("O maior número é: $maiorNum")
+
+
+}
