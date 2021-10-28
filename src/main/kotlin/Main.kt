296ecc6329c04f3f0afb4ce57857a7b1932b4c72
Ademir Oliveira <ademir.sojunior@gmail.com>
2021-10-28 09:04:03 -0300

    alteração no código para achar o menor número

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,7 +10,7 @@ fun main(args: Array<String>) {
      */
 
     var soma = 0
-    var contador = 0.0
+    var contador = 0
     var media = 0.0
     var menorNum = 0
     var maiorNum = 0
@@ -18,27 +18,31 @@ fun main(args: Array<String>) {
     do {
         print("Digite um número inteiro e positivo: ")
         var numero = readLine()!!.toInt()
-        contador++
-        soma += numero
-        if (numero == 0){
-            contador--
-        }
-        media = soma/contador
 
-        if (numero!=0 && numero < menorNum){
-            menorNum = numero
-        }
-        if (numero > maiorNum){
-            maiorNum = numero
+        if (numero != 0){
+            soma += numero
 
+            if(contador == 0){
+                maiorNum = numero
+                menorNum = numero
+            }else{
+                if (numero > maiorNum){
+                    maiorNum = numero
+                }
+                if (numero < menorNum){
+                    menorNum = numero
+                }
+            }
+            contador++
         }
 
     }while (numero != 0)
-    println("$contador")
+
+    media = soma/contador.toDouble()
+
     println("A soma dos números é: $soma")
     println("A média dos números é: $media")
     println("O menor número é: $menorNum")
     println("O maior número é: $maiorNum")
-
-
+    
 }
